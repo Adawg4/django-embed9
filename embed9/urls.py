3daa7eb4c57c77a8c56a8779a6e06e0e203d94ae
Aidan <27599708+Adawg4@users.noreply.github.com>
2019-11-29 14:16:02 -0500

    Update urls.py
    Source:
    Common sense

--- embed9/urls.py
+++ embed9/urls.py
@@ -3,8 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from embed9 import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     path('widget/<app>/<model>/<pk>/', views.widget, name='widget'),
     path('loader/<app>/<model>/<pk>/', views.loader, name='loader'),
     path('preview/<app>/<model>/<pk>/', csrf_exempt(views.preview), name='preview'),
-)
+]
