959afb3a32f3571f04dc5eb20d536ef2a90887ee
Aidan <27599708+Adawg4@users.noreply.github.com>
2019-12-02 08:14:10 -0500

    Fix request context
    Source:
    Common sense
    Also:
    https://stackoverflow.com/questions/43787700/django-1-11-typeerror-context-must-be-a-dict-rather-than-context#43787762

--- embed9/views.py
+++ embed9/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from django.contrib.sites.models import Site
 from django.urls import reverse
@@ -16,14 +16,13 @@ WIDGET_CACHE_TIME = getattr(settings, 'WIDGET_CACHE_TIME', 60*60)
 @xframe_options_exempt
 def widget(request, app, model, pk):
     """ Renders an iframe with the widget. """
-    embed, obj = common_view(app, model, pk)
     params = get_params(embed.get_form_class(), request.GET)
     template = embed.get_widget_template()
     
-    return render_to_response(template, {
+    return render(request, template, {
         model: obj,
         'params': params,
-    }, RequestContext(request))
+    },)
 
 
 @cache_page(WIDGET_CACHE_TIME)
@@ -34,13 +33,13 @@ def loader(request, app, model, pk):
     params = get_params(embed.get_form_class(), request.GET)
     template = embed.get_loader_template()
 
-    return render_to_response(template, {
+    return render(request, template, {
         model: obj,
         'widget_name': 'widget_' + model + str(pk),
         'domain': Site.objects.get_current().domain,
         'iframe_url': mark_safe(reverse('embed9:widget', kwargs={'app': app, 'model': model, 'pk': pk}) + get_encoded_params(params)),
         'params': params,
-    }, RequestContext(request))
+    })
 
 
 def preview(request, app, model, pk):
@@ -60,9 +59,9 @@ def preview(request, app, model, pk):
     else:
         form = embed.get_form_class()()
     
-    return render_to_response(template, {
+    return render(request, template, {
         'obj': obj,
         'form': form,
         'params': params,
         'show_preview': show_preview,
-    }, RequestContext(request))
+    },)
